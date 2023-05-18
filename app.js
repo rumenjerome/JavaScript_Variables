a390332a963bea9dad583293c4b3da0324128ed2
Rumen Jerome <rumen3435@gmail.com>
2023-05-18 09:54:43 -0700

    Added variables

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Rumen";
+let lastName = "Jerome";
+var age = "23"
 
-// Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true
+
+console.log()
